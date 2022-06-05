67cef37f32ed5a9fe223e0d4b025c329ec4e8631
Hassan <hakhan0301@gmail.com>
2022-06-05 14:35:41 -0500

    minur

--- webapp/pages/login.tsx
+++ webapp/pages/login.tsx
@@ -4,7 +4,7 @@ export default function LoginPage() {
   return (
     <div className="py-6 text-white">
       <div className="flex flex-col sm:flex-row gap-6 justify-center items-center">
-        <div className="bg-gradient-to-br from-orange-400 to-pink-500 rounded-lg shadow-lg text-black">
+        <div className="bg-gradient-to-br from-orange-400 to-pink-500 rounded-lg shadow-lg text-white">
           <div className="flex flex-col gap-4 p-6 pb-6 motion-safe:animate-spin hover:animate-none">
             <h1 className="text-3xl">Login</h1>
             <div>
