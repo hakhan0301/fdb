11282a8c0dc3e8ef82061009b11a53732ed59848
Hassan <hakhan0301@gmail.com>
2022-06-09 02:20:11 -0500

    react aria

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -2,27 +2,38 @@ import NavBar from '../lib/NavBar';
 import '../styles/globals.css';
 import Head from 'next/head';
 import { SessionProvider } from "next-auth/react";
+import { SSRProvider } from '@react-aria/ssr';
+
 import type { AppProps } from 'next/app';
 
 function MyApp({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
-  return (
-    <SessionProvider session={session}>
-      <Head>
-        <title>Foolar Daily Blog - FDB</title>
-        <meta name="description" content="FOOLARS ASSEMBLE" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
+  const Header = () => (
+    <Head>
+      <title>Foolar Daily Blog - FDB</title>
+      <meta name="description" content="FOOLARS ASSEMBLE" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+
+
+  return (
+    <>
+      <Header />
       <main className="h-screen flex flex-col">
-        <NavBar />
-        <div className='flex-grow'>
-          <Component {...pageProps} />
-        </div>
+        <SessionProvider session={session}>
+          <SSRProvider>
+            <NavBar />
+            <div className='flex-grow'>
+              <Component {...pageProps} />
+            </div>
+          </SSRProvider>
+        </SessionProvider>
       </main>
-    </SessionProvider>
+    </>
   );
 }
 
