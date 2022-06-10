cf19a2102558d25675619858faabff5c053d8ab9
Hassan <hakhan0301@gmail.com>
2022-06-09 22:34:34 -0500

    tabs & styling

---
+++ packages/ui/common/Tabs.tsx
@@ -0,0 +1,62 @@
+import { Item } from '@react-stately/collections';
+import { useTab, useTabList, useTabPanel } from '@react-aria/tabs';
+import { useTabListState } from '@react-stately/tabs';
+import { useRef } from 'react';
+import type { TabListProps } from '@react-types/tabs';
+
+export function Tabs(props: TabListProps<object>) {
+  let state = useTabListState(props);
+  let ref = useRef();
+  let { tabListProps } = useTabList(props, state, ref);
+  return (
+    <div className=''>
+      <div
+        className='flex gap-1'
+        {...tabListProps}
+        ref={ref}
+      >
+        {[...state.collection].map((item) => (
+          <Tab key={item.key} item={item} state={state} />
+        ))}
+      </div>
+      <TabPanel key={state.selectedItem?.key} state={state} />
+    </div>
+  );
+}
+
+function Tab({ item, state }) {
+  let { key, rendered } = item;
+  let ref = useRef();
+  let { tabProps } = useTab({ key }, state, ref);
+  let isSelected = state.selectedKey === key;
+  let isDisabled = state.disabledKeys.has(key);
+  return (
+    <div
+      {...tabProps}
+      ref={ref}
+      className={`px-2 py-1 select-none cursor-pointer 
+        w-fit
+        border-b border-rose-800
+        text-black bg-gradient-to-t from-yellow-300 to-pink-300
+        w-[100%]
+        ${isSelected ? 'border-rose-800 border-b-4' : ''}`}
+      style={{
+        opacity: isDisabled ? '0.5' : undefined
+      }}
+    >
+      {rendered}
+    </div>
+  );
+}
+
+function TabPanel({ state, ...props }) {
+  let ref = useRef();
+  let { tabPanelProps } = useTabPanel(props, state, ref);
+  return (
+    <div {...tabPanelProps} ref={ref}>
+      {state.selectedItem?.props.children}
+    </div>
+  );
+}
+
+export const TabItem = Item;
--- packages/ui/posts/NewPostField.tsx
+++ packages/ui/posts/NewPostField.tsx
@@ -3,11 +3,13 @@ import Button from "@fdb/ui/common/Button";
 import { useRouter } from 'next/router';
 import { useState } from "react";
 
+import { Tabs, TabItem } from "../common/Tabs";
+
 export default function NewPostField({ }: any) {
   const router = useRouter();
 
   const [postValue, setPostValue] = useState('');
-  const [submitting, setSubmitting] = useState(false)
+  const [submitting, setSubmitting] = useState(false);
 
   const submitBlogPost = async () => {
     setSubmitting(true);
@@ -23,15 +25,22 @@ export default function NewPostField({ }: any) {
   };
 
   return (
-    <div className='flex flex-col bg-emerald-100 p-4'>
-      <div className='px-1px'>Blog Text</div>
-      <div className='flex flex-col md:flex-row items-start gap-2'>
-        <TextArea
-          className="flex-grow w-[100%]"
-          value={postValue} onChange={setPostValue}
-        />
-        <Button onPress={submitBlogPost} isDisabled={submitting}>Post</Button>
-      </div>
+    <div className='bg-emerald-100 p-4'>
+      <Tabs >
+        <TabItem title="Text">
+          <div className="bg-yellow-50 p-2 pb-3">
+            <div className='px-1px'>Blog Text</div>
+            <div className='flex flex-col md:flex-row items-start gap-2'>
+              <TextArea
+                className="flex-grow w-[100%]"
+                value={postValue} onChange={setPostValue}
+              />
+              <Button onPress={submitBlogPost} isDisabled={submitting}>Post</Button>
+            </div>
+          </div>
+        </TabItem>
+        <TabItem title="susy2">fdsa</TabItem>
+      </Tabs>
     </div>
   )
 }
