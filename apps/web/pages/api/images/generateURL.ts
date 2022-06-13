30e59ea0f48fe919020591afdc4f369d6efb14f6
Hassan <hakhan0301@gmail.com>
2022-06-12 23:35:48 -0500

    generate s3 url & upload image

---
+++ apps/web/pages/api/images/generateURL.ts
@@ -0,0 +1,40 @@
+import { v4 as uuidv4 } from 'uuid';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import { S3 } from 'aws-sdk';
+
+
+const s3 = new S3({
+  region: process.env.IMAGE_BUCKET_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+
+async function generateS3Url(): Promise<string> {
+  return s3.getSignedUrl('putObject', {
+    Bucket: process.env.IMAGE_BUCKET,
+    Key: uuidv4(),
+  });
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { method } = req;
+
+  try {
+    switch (method) {
+      case 'GET':
+        return GET_generateImageUrl(req, res);
+      default:
+        return res.status(404).json('Route not found.');
+    }
+  } catch (e) {
+    console.error(e);
+
+    return res.status(500).json({ error: 'Internal Server Error.' });
+  }
+}
+
+async function GET_generateImageUrl(req: NextApiRequest, res: NextApiResponse) {
+  const s3Url = await generateS3Url();
+  return res.status(200).send(s3Url);
+}
--- packages/ui/posts/NewPostField.tsx
+++ packages/ui/posts/NewPostField.tsx
@@ -14,11 +14,50 @@ export default function NewPostField({ }: any) {
       <Tabs >
         <TabItem title="Text"><TextContentField /></TabItem>
         <TabItem title="Link"><LinkContentField /></TabItem>
+        <TabItem title="Image"><ImageContentField /></TabItem>
       </Tabs>
     </div>
   )
 }
 
+
+function TextContentField({ }: any) {
+  const router = useRouter();
+
+  const [postValue, setPostValue] = useState('');
+  const [submitting, setSubmitting] = useState(false);
+
+  const isValidText = !(postValue.length < 3 || postValue.length > 300);
+
+  const submitBlogPost = async () => {
+    if (!isValidText) return;
+    setSubmitting(true);
+    await fetch('/api/blogs', {
+      method: 'POST',
+      body: JSON.stringify({
+        type: 'text', body: postValue
+      })
+    });
+    setPostValue('');
+    setSubmitting(false);
+    router.push('/');
+  };
+
+
+  return (
+    <div className="bg-yellow-50 p-4">
+      <div className='flex flex-col items-end gap-3'>
+        <TextArea
+          label="Post Text"
+          className="flex-grow w-[100%]"
+          value={postValue} onChange={setPostValue}
+          errorMessage={!isValidText}
+        />
+        <Button onPress={submitBlogPost} isDisabled={submitting}>Post</Button>
+      </div>
+    </div>
+  );
+}
 
 function LinkContentField({ }: any) {
   const router = useRouter();
@@ -72,39 +111,50 @@ function LinkContentField({ }: any) {
   );
 }
 
-function TextContentField({ }: any) {
-  const router = useRouter();
 
-  const [postValue, setPostValue] = useState('');
+function ImageContentField({ }: any) {
+  const router = useRouter();
   const [submitting, setSubmitting] = useState(false);
 
-  const isValidText = !(postValue.length < 3 || postValue.length > 300);
+  //create image usestate
+  const [imageFile, setImageFile] = useState<File | null>(null);
 
-  const submitBlogPost = async () => {
-    if (!isValidText) return;
-    setSubmitting(true);
-    await fetch('/api/blogs', {
-      method: 'POST',
-      body: JSON.stringify({
-        type: 'text', body: postValue
-      })
+  const submitImage = async () => {
+    if (!imageFile) return;
+
+    const [bytes, url] = await Promise.all([
+      imageFile.arrayBuffer(),
+      fetch('/api/images/generateURL').then(res => res.text())
+    ]);
+
+    await fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      },
+      body: imageFile
     });
-    setPostValue('');
-    setSubmitting(false);
-    router.push('/');
+
+
+
+    console.log(bytes, url);
+
+
   };
+
+  const onImage = (imageFile: File | null) => {
+    setImageFile(imageFile);
+  }
 
 
   return (
     <div className="bg-yellow-50 p-4">
       <div className='flex flex-col items-end gap-3'>
-        <TextArea
-          label="Post Text"
-          className="flex-grow w-[100%]"
-          value={postValue} onChange={setPostValue}
-          errorMessage={!isValidText}
-        />
-        <Button onPress={submitBlogPost} isDisabled={submitting}>Post</Button>
+        <div className="w-[100%]">
+          <input type="file" accept="image/*"
+            onChange={(e) => onImage(e.target.files && e.target.files[0])} />
+        </div>
+        <Button onPress={submitImage} isDisabled={submitting}>Post</Button>
       </div>
     </div>
   );
