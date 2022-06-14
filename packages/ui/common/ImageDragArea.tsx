23c8f562f640c5e69a1560c5ed65daa8099f30e2
Hassan <hakhan0301@gmail.com>
2022-06-14 01:58:12 -0500

    scuffed ass mobile images

--- packages/ui/common/ImageDragArea.tsx
+++ packages/ui/common/ImageDragArea.tsx
@@ -3,6 +3,7 @@ import ModalImage from "react-modal-image-responsive";
 import { AiOutlineClose, AiOutlinePlus } from 'react-icons/ai';
 import { useEffect, useState } from "react";
 import { useDropzone } from 'react-dropzone';
+import { BrowserView, MobileView } from 'react-device-detect';
 
 interface Props {
   onChange?: ((image: File) => void) | null
@@ -14,6 +15,7 @@ export default function ImageDragArea({ onChange = null }: Props = {}) {
 
   const { getRootProps, getInputProps, isFocused, acceptedFiles } = useDropzone({
     multiple: false,
+
     maxSize: 10000000,
     accept: { 'image/*': [] },
   });
@@ -35,35 +37,45 @@ export default function ImageDragArea({ onChange = null }: Props = {}) {
       onChange(acceptedFiles[0]);
   }, [acceptedFiles, onChange]);
 
-  if (imagePreview)
-    return (
-      <div className={`flex flex-col p-2 gap-1 items-end w-[100%] bg-pink-100 border border-pink-300 shadow-lg`}>
-        <AiOutlineClose
-          className="cursor-pointer hover:text-red-600"
-          onClick={() => setImageFile(null)}
-        />
-        <div className="flex justify-center w-full h-auto">
-          <ModalImage
-            className="w-full h-auto"
-            small={imagePreview}
-            large={imagePreview}
-            hideDownload hideZoom
-          />
-        </div>
-      </div>
-    );
-
   return (
-    <div className="w-full shadow-lg">
-      <div {...getRootProps({ className: 'w-[100%]', onChange: console.log })}>
-        <div className={`flex items-center justify-center bg-pink-100 p-10 cursor-pointer
-          ${isFocused ? '' : 'hover:border-emerald-200'}
-          border-dashed border-2 ${isFocused ? 'border-emerald-400' : 'border-red-400'}`}
-        >
-          <input {...getInputProps()} onChange={() => console.log('susy')} />
-          <AiOutlinePlus className={`${isFocused ? 'text-emerald-400' : 'text-red-400'} text-2xl`} />
+    <div className="w-full">
+      <BrowserView>
+        {imagePreview
+
+          ? (<div className={`flex flex-col p-2 gap-1 items-end w-[100%] bg-pink-100 border border-pink-300 shadow-lg`}>
+            <AiOutlineClose
+              className="cursor-pointer hover:text-red-600"
+              onClick={() => setImageFile(null)}
+            />
+            <div className="flex justify-center w-full h-auto">
+              <ModalImage
+                className="w-full h-auto"
+                small={imagePreview}
+                large={imagePreview}
+                hideDownload hideZoom
+              />
+            </div>
+          </div>)
+          : (<div className="w-full shadow-lg">
+            <div {...getRootProps({ className: 'w-[100%]', onChange: console.log })}>
+              <div className={`flex items-center justify-center bg-pink-100 p-10 cursor-pointer
+        ${isFocused ? '' : 'hover:border-emerald-200'}
+        border-dashed border-2 ${isFocused ? 'border-emerald-400' : 'border-red-400'}`}
+              >
+                <input {...getInputProps()} onChange={() => console.log('susy')} />
+                <AiOutlinePlus className={`${isFocused ? 'text-emerald-400' : 'text-red-400'} text-2xl`} />
+              </div>
+            </div>
+          </div>)
+        }
+      </BrowserView>
+
+      <MobileView>
+        <div>
+          <input type="file" accept="image/*"
+            onChange={(e) => setImageFile(e.target.files && e.target.files[0])} />
         </div>
-      </div>
+      </MobileView>
     </div>
   );
 }
