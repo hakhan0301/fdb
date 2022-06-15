a468ca6790db3caeb1a01d45d6f90149619664e6
Hassan <hakhan0301@gmail.com>
2022-06-14 23:23:58 -0500

    streakstrike popup

--- packages/ui/posts/StreakStrike.tsx
+++ packages/ui/posts/StreakStrike.tsx
@@ -1,18 +1,56 @@
 import { AiFillFire } from 'react-icons/ai';
 import { BsExclamationDiamond } from 'react-icons/bs';
+import Popup from 'reactjs-popup';
+import './styles/StreakStrike.css';
+
+
+function StreakInfoPopup() {
+  return (
+    <div className='bg-black text-fuchsia-300 rounded-md '>
+      <div className='p-2 text-center'>
+        Your current streak.
+      </div>
+    </div>
+  );
+};
+
+
+function StrikesInfoPopup() {
+  return (
+    <div className='bg-black text-fuchsia-300 rounded-md '>
+      <div className='p-2 text-center'>
+        You must post every day. <br />
+      </div>
+    </div>
+  );
+};
 
 export default function StreakStrike({ strikes, streak }: { streak: number, strikes: number }) {
   return (
     <div className='bg-gradient-to-r from-pink-400 to-purple-500'>
       <div className='flex justify-between text-3xl p-2'>
         <div className='flex items-center select-none'>
-          <AiFillFire className='text-black w-8 h-8 fill-fuchsia-800 animate-pulse' />
+
+          <Popup position="bottom center" trigger={
+            <button>
+              <AiFillFire className='text-black w-8 h-8 fill-fuchsia-800 animate-pulse' />
+            </button>} >
+            <StreakInfoPopup />
+          </Popup>
+
           <span className='font-serif text-fuchsia-900'>{streak}</span>
         </div>
 
 
         <div className='flex gap-1 items-center select-none font-serif text-black'>
-          <BsExclamationDiamond className='h-7 w-7 pr-1' />
+
+          <Popup position="bottom center" trigger={
+            <button>
+              <BsExclamationDiamond className='h-7 w-7 pr-1' />
+            </button>} >
+            <StrikesInfoPopup />
+          </Popup>
+
           <span>{strikes}</span>
           <span className='font-extrabold'>/</span>
           <span>3</span>
