959fd7f8b60d1bfb71316afcfce2a015e16ca130
Hassan <hakhan0301@gmail.com>
2022-06-14 19:13:56 -0500

    test page bug

--- apps/web/pages/test.tsx
+++ apps/web/pages/test.tsx
@@ -1,7 +1,8 @@
 import { useSession } from "next-auth/react";
 
 export default function Component() {
-  const session = useSession()
+  const session = useSession();
+
 
   return (
     <div className="flex h-[100%] flex-col gap-12 p-12 bg-slate-700 text-white">
@@ -9,7 +10,7 @@ export default function Component() {
         session: {JSON.stringify(session, null, 2)}
       </pre>
       <pre className="bg-purple-900 p-6 overflow-auto">
-        window.location.hostname: {window.location.hostname}
+        window.location.hostname: {session.data && window?.location?.hostname}
       </pre>
     </div>
   );
