beff05ff55539e43fd98b726880ac9a3f6770817
Hassan <hakhan0301@gmail.com>
2022-06-16 22:18:53 -0500

    with pwa

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -4,8 +4,9 @@ const withTM = require("next-transpile-modules")([
   "@fdb/ui",
   "@fdb/db"
 ]);
+const withPWA = require('next-pwa');
 
 module.exports = withPlugins([
+  withPWA({ pwa: { dest: 'public' } }),
   withTM({ reactStrictMode: true, }),
-
 ]);
