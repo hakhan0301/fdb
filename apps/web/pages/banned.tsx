e94689121e19bdf2ccb0aa34e2dc5e115b71ce32
Hassan <hakhan0301@gmail.com>
2022-06-16 21:08:21 -0500

    updated ban screen

--- apps/web/pages/banned.tsx
+++ apps/web/pages/banned.tsx
@@ -3,13 +3,13 @@ import { MdGavel } from 'react-icons/md';
 
 export default function BannedPage() {
   return (
-    <div className="h-full bg-red-500">
+    <div className="h-full w-full bg-red-500">
       <div className="h-full flex gap-4 flex-col content-center justify-center items-center">
         <MdGavel className="flex-no-shrink fill-current w-16 h-16" />
-        <div className="text-3xl font-bold text-center">
-          Your account has been banned <br />
+        <p className="text-3xl font-bold text-center max-w-xl">
+          Your account has been banned
           for missing too many daily posts.
-        </div>
+        </p>
       </div>
     </div>
   )
